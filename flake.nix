4cf5656a246ad9d66edb7aafe88422632afdc3db
Babbaj <babbaj45@gmail.com>
2022-05-25 01:29:56 -0400

    copy shell config from desktop config

--- flake.nix
+++ flake.nix
@@ -12,12 +12,43 @@
         url = "github:leijurv/gb?ref=pull/21/head";
         flake = false;
     };
+    inputs.home-manager.url = "github:nix-community/home-manager";
+    inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    inputs.nixos-config.url = "github:babbaj/nix-config";
 
-    outputs = { self, nixpkgs, deploy-rs, agenix, gb-src, gb-pr21-src }:
-    {
+    outputs = { self, nixpkgs, deploy-rs, agenix, gb-src, gb-pr21-src, home-manager, nixos-config }:
+    let
+        home-module = {
+            home-manager = {
+                users.root = {
+                    imports = [
+                        # TODO: organize these into "shell config" list
+                        "${nixos-config}/home/zsh.nix"
+                        "${nixos-config}/home/bash.nix"
+                        "${nixos-config}/home/fzf.nix"
+                        "${nixos-config}/home/htop.nix"
+                        "${nixos-config}/home/starship.nix"
+                        "${nixos-config}/home/zoxide.nix"
+                    ];
+                };
+                useUserPackages = true;
+                useGlobalPkgs = true;
+                verbose = true;
+            };
+        };
+
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
         nixosConfigurations.hetzner-system = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
-            modules = [ ./hetzner-hardware.nix ./configuration.nix agenix.nixosModules.age ];
+            modules = [
+                ./hetzner-hardware.nix
+                ./configuration.nix
+                agenix.nixosModules.age
+                home-manager.nixosModules.home-manager
+                home-module
+            ];
+            inherit pkgs;
             specialArgs = {
                 inherit gb-src;
                 inherit gb-pr21-src;
